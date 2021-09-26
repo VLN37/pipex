dc06bf32b45d2219f93aaf4f127fd64d0ad5a894
VLN37 <54167882+VLN37@users.noreply.github.com>
2021-09-26 16:35:31 -0300

    feat: command parser working

--- pipex.c
+++ pipex.c
@@ -6,19 +6,28 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 22:17:58 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/09/25 23:36:24 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/09/26 16:34:23 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include "libft.h"
 
-int	main(void)
+char	***parse_commands(int argc, char **argv, char ***cmds)
 {
-	int	file_in;
-	int	file_out;
-	int	pipefd[2];
-	int	pid1;
-	int	pid2;
-	char	*path;
-	char	**cmd1;
+	int	i;
+
+	i = -1;
+	cmds = malloc(sizeof(char **) * argc - 3 + 1);
+	while (++i < argc - 3)
+		cmds[i] = ft_split(argv[i + 2], ' ');
+	cmds[i] = NULL;
+	return (cmds);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+
+	data.cmds = parse_commands(argc, argv, data.cmds);
 }
--- pipex.h
+++ pipex.h
@@ -6,27 +6,27 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 00:51:03 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/09/26 14:31:19 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/09/26 16:35:04 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <stdio.h>
+# include <stdio.h>
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdlib.h>
 
-typedef	struct s_data
+typedef struct s_data
 {
 	int		file_in;
 	int		file_out;
 	int		pipefd[2];
 	int		pid;
-	char	*path;
+	char	**path;
 	char	***cmds;
 }	t_data;
 
